312d595a44a69210d2757e78f5ecdb44ce922970
Devendra <devendrapandey125@gmail.com>
2026-09-13 14:48:47 +0530

    [Devendra]: fastapi get posts route

--- main.py
+++ main.py
@@ -2,10 +2,29 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+posts: list[dict] = [
+{
+"id" : 1,
+"author": "Dev DKP",
+"title": "FastAPI is Amazing",
+"content": "This framework is indeed super easy, cool and super fast.",
+"date_posted": "September 12, 2026"
+},
+{
+"id" : 2,
+"author": "Devendra DKP",
+"title": "Python is Great for Web Development",
+"content": "Python is a great language for web development, and FastAPI makes it even better.",
+"date_posted": "September 13, 2026"
+}
+]
+
 
 @app.get("/")
 def home():
     return {"message": "Hello FastAPI World!"}
 
 
-# print("Hello FastAPI World!")
+@app.get("/api/posts")
+def get_posts():
+    return posts
